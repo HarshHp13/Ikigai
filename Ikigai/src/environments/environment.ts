ca956e49e77f903fb00db4ca742c3dfcc1309877
Harsh <84851169+HarshHp13@users.noreply.github.com>
2023-03-22 01:20:06 +0530

    firebase integration

---
+++ Ikigai/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ikigai-d17ca',
+    appId: '1:550920877561:web:14164fb6d5c0fa130c3928',
+    storageBucket: 'ikigai-d17ca.appspot.com',
+    apiKey: 'AIzaSyDPXw-ot8BGMGlnYpuxprf0E0QISsQjQ50',
+    authDomain: 'ikigai-d17ca.firebaseapp.com',
+    messagingSenderId: '550920877561',
+    measurementId: 'G-M0NY8FWRKP',
+  },};
